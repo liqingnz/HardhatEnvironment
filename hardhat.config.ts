45591054b10bfb05996cf23d8b4b50a8f61553a9
Li-Qing Wang <liqingnz@gmail.com>
2023-12-27 15:15:10 +0800

    add package, update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,34 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+    solidity: {
+        version: "0.8.19",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
+    },
+    networks: {
+        hardhat: {},
+        mumbai: {
+            url: "https://polygon-mumbai.g.alchemy.com/v2/BSCzQY85ap-o5po_HdIQuUHQbnFQLNXS",
+            accounts: [process.env.PRIVATE_KEY!],
+        },
+        polygon: {
+            url: "https://polygon-mainnet.g.alchemy.com/v2/h96rDJZ9p0LJKDT5bQsAbm7iaj30JTQ6",
+            accounts: [process.env.PRIVATE_KEY!],
+        },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_KEY,
+    },
 };
 
 export default config;
